1363f0c3dc8fa90f5d94582bc4bf4f884a6998e1
Matias Christensen <mattivc@gmail.com>
2012-07-18 19:09:47 +0200

    Fixed unnecessary property use in FlameController

--- Assets/Scripts/Editor/FlamerControllerEditor.cs
+++ Assets/Scripts/Editor/FlamerControllerEditor.cs
@@ -8,9 +8,17 @@ public class FlamerControllerEditor : Editor {
 	private FlamerController editorTarget = null;
 	private float fireLength;
 	
+	private BoxCollider flameCollider;
+	private ParticleEmitter flameParticles;
+	
 	void OnEnable ()
 	{
 		editorTarget = target as FlamerController;
+		
+		flameCollider = editorTarget.gameObject.GetComponent<BoxCollider>();
+		flameParticles = editorTarget.gameObject.GetComponentInChildren<ParticleEmitter>();
+		
+		Debug.Log(flameCollider + " - " + flameParticles);
 	}
 	
 	void OnDisable ()
@@ -18,25 +26,27 @@ public class FlamerControllerEditor : Editor {
 		
 	}
 	
-	public override void OnInspectorGUI () // TODO Check this method when less tired
+	public override void OnInspectorGUI ()
 	{
-		editorTarget.fireLength = (float)EditorGUILayout.IntSlider( Mathf.RoundToInt(editorTarget.fireLength), 1, 8 );
+		editorTarget.fireLength = (float)EditorGUILayout.IntSlider(Mathf.RoundToInt(editorTarget.fireLength), 1, 8 );
 		
-		if( GUI.changed )
-		{	
-			Vector3 oldColliderPos = editorTarget.FlameCollider.center;
-			Vector3 oldColliderSize = editorTarget.FlameCollider.size;
+		if (GUI.changed)
+		{
+			Undo.RegisterUndo(new Object[]{editorTarget, flameCollider, flameParticles}, "Changed flame length");
 			
-			Vector3 newColliderPos = new Vector3( ( editorTarget.fireLength * 0.5f ) + 1f , oldColliderPos.y, oldColliderPos.z );
-			Vector3 newColliderSize = new Vector3( editorTarget.fireLength, oldColliderSize.y, oldColliderSize.z );
+			Vector3 oldColliderPos = flameCollider.center;
+			Vector3 oldColliderSize = flameCollider.size;
 			
-			float particleSpeed = editorTarget.FlameParticles.localVelocity.x;
+			Vector3 newColliderPos = new Vector3((editorTarget.fireLength * 0.5f ) + 1f , oldColliderPos.y, oldColliderPos.z);
+			Vector3 newColliderSize = new Vector3(editorTarget.fireLength, oldColliderSize.y, oldColliderSize.z);
 			
-			editorTarget.FlameParticles.maxEnergy = ( editorTarget.fireLength + 0.5f ) / particleSpeed;
-			editorTarget.FlameParticles.minEnergy = ( editorTarget.fireLength - 0.5f ) / particleSpeed;
+			float particleSpeed = flameParticles.localVelocity.x;
 			
-			editorTarget.FlameCollider.center = newColliderPos;
-			editorTarget.FlameCollider.size = newColliderSize;
+			flameParticles.maxEnergy = (editorTarget.fireLength + 0.5f) / particleSpeed;
+			flameParticles.minEnergy = (editorTarget.fireLength - 0.5f) / particleSpeed;
+			
+			flameCollider.center = newColliderPos;
+			flameCollider.size = newColliderSize;
 			
 			EditorUtility.SetDirty(editorTarget);
 		}
