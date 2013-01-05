c960a3c0f9425e4d615d1d177fce2333c438c059
Matias Christensen <mattivc@gmail.com>
2013-01-05 17:33:41 +0100

    Changed MD5Util to accept byte arrays.

--- Assets/ISFramework/Scripts/Data/MD5Util.cs
+++ Assets/ISFramework/Scripts/Data/MD5Util.cs
@@ -7,9 +7,14 @@ public static class MD5Util
 		System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding ();
 		byte[] bytes = ue.GetBytes (strToEncrypt);
 		
+		return Md5Sum(bytes);
+	}
+	
+	public static string Md5Sum (byte[] dataToEncrypt)
+	{
 		// encrypt bytes
 		System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider ();
-		byte[] hashBytes = md5.ComputeHash (bytes);
+		byte[] hashBytes = md5.ComputeHash (dataToEncrypt);
 		
 		// Convert the encrypted bytes back to a string (base 16)
 		string hashString = "";
