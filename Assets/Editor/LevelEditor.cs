e6bdba2f03fcf86b71775555d30d71c47b8a7047
Matias Christensen <mattivc@gmail.com>
2013-02-03 19:48:09 +0100

    Major rewrite of Level Editor

--- Assets/Editor/LevelEditor.cs
+++ Assets/Editor/LevelEditor.cs
@@ -6,19 +6,19 @@ using System.IO;
 
 public class LevelEditor : EditorWindow {
 	
-	public class LevelItem : Object {
+	private const float UISize = 20f;
+	
+	public class PrefabItem : Object {
 		public string itemName;
+		public string parent;
+		public GameObject prefab;
 		
 		private Texture2D _icon = null;
-		public Texture2D icon
-		{
-			get
-			{
-				do
-				{	
+		public Texture2D icon {
+			get {
+				do { // TODO: Fix potential infinite loop
 					_icon = AssetPreview.GetAssetPreview(this.prefab);
-				}
-				while (AssetPreview.IsLoadingAssetPreviews());
+				} while (AssetPreview.IsLoadingAssetPreviews());
 				
 				if (this._icon == null)
 					_icon = (Texture2D)AssetDatabase.LoadAssetAtPath(@"Assets/Editor/Icons/missingIcon.png", typeof(Texture2D));
@@ -26,8 +26,6 @@ public class LevelEditor : EditorWindow {
 				return _icon;
 			}
 		}
-		
-		public GameObject prefab;
 	}
 	
 	private static LevelEditor _window = null;
@@ -43,10 +41,10 @@ public class LevelEditor : EditorWindow {
 		}
 	}
 	
-	private Vector2 scrollPos;
+	private int toolbarSelected = 0;
 	
-	private List<LevelItem> itemPrefabs;
-	private List<LevelItem> levelPrefabs;
+	private List<PrefabItem> itemPrefabs;
+	private List<PrefabItem> levelPrefabs;
 	
 	[MenuItem ("Tools/Level Editor")]
 	static void Init()
@@ -56,35 +54,35 @@ public class LevelEditor : EditorWindow {
 	
 	void OnEnable ()
 	{
-		scrollPos = Vector2.zero;
 		Refresh();
 	}
 	
 	private void Refresh()
 	{
-		itemPrefabs = new List<LevelItem>();
+		itemPrefabs = new List<PrefabItem>();
 		string[] itemPrefabPaths = Directory.GetFiles((@"Assets\Prefabs\Items\").Replace(@"\", Path.AltDirectorySeparatorChar.ToString()), "*.prefab");
 		
 		foreach( string objectPath in itemPrefabPaths ) // Build list of item prefabs
 		{			
-			LevelItem newItemObject = new LevelItem();
+			PrefabItem newItemObject = new PrefabItem();
 			
 			newItemObject.prefab = (GameObject)AssetDatabase.LoadAssetAtPath(objectPath, typeof(GameObject) );
 			newItemObject.itemName = newItemObject.prefab.name;
+			newItemObject.parent = "items";
 			
 			itemPrefabs.Add(newItemObject);
 		}
 		
-		levelPrefabs = new List<LevelItem>();
+		levelPrefabs = new List<PrefabItem>();
 		string[] levelPrefabPaths = Directory.GetFiles((@"Assets\Prefabs\Level\").Replace(@"\", Path.AltDirectorySeparatorChar.ToString()), "*.prefab");
 		
 		foreach( string objectPath in levelPrefabPaths ) // Build list of level prefabs
 		{			
-			LevelItem newLevelObject = new LevelItem();
+			PrefabItem newLevelObject = new PrefabItem();
 			
 			newLevelObject.prefab = (GameObject)AssetDatabase.LoadAssetAtPath(objectPath, typeof(GameObject) );
 			newLevelObject.itemName = newLevelObject.prefab.name;
-
+			newLevelObject.parent = "level";
 			
 			levelPrefabs.Add(newLevelObject);
 		}
@@ -92,39 +90,195 @@ public class LevelEditor : EditorWindow {
 	
 	void OnGUI ()
 	{
-		scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+		
 		DrawToolButtons();
-		DrawComponentButtons();
-		DrawLevelIntegrity();
-		DrawItemButtons();
-		DrawLevelButtons();
-		GUI.enabled = true;
-		EditorGUILayout.EndScrollView();
+		
+		
+		if (IsLevelInitialized()) {
+			toolbarSelected = GUI.Toolbar(new Rect(1f, 11f, 18f, 1f).Multiply(UISize), toolbarSelected, new string[]{"Level", "Items", "Components", "Properties"});
+			
+			switch (toolbarSelected) {
+			case 0:
+				DrawLevelButtons();
+				break;
+			case 1:
+				DrawItemButtons();
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			}
+			
+		} else {
+			if (GUI.Button(new Rect(1f, 11f, 18f, 1f).Multiply(UISize), new GUIContent("Initialize Level"))) {
+				InitializeLevel();
+			}
+		}
 	}
 	
-	private void DrawLevelIntegrity()
+	
+	private void MoveSelectedObjects(Vector3 displacement) {
+		foreach (GameObject selected in Selection.gameObjects) {
+			Vector3 oldPos = selected.transform.localPosition;
+			Vector3 newPos = oldPos + displacement;
+			selected.transform.localPosition = newPos;
+		}
+	}
+	
+	private void RotateSelectedObjects(Vector3 rotation) {
+		foreach (GameObject selected in Selection.gameObjects) {
+			Vector3 oldRot = selected.transform.localEulerAngles;
+			Vector3 newRot = oldRot + new Vector3(	Mathf.Repeat(rotation.x, 360f),
+													Mathf.Repeat(rotation.y, 360f),
+													Mathf.Repeat(rotation.z, 360f)	);
+			selected.transform.localEulerAngles = newRot;
+		}
+	}
+	
+	private void RotateSelectedUVs() {
+		Undo.RegisterUndo(Selection.gameObjects, "Rotate UVs");
+		
+		foreach( GameObject go in Selection.gameObjects )
+		{
+			MeshFilter meshFilter = go.GetComponentInChildren<MeshFilter>();
+			Mesh mesh = (Mesh)Instantiate(meshFilter.sharedMesh);
+			
+			Vector2[] uvArray = mesh.uv;
+			
+			for (int i = 0; i < uvArray.Length; i++)
+			{
+				uvArray[i] = Quaternion.Euler(0f, 0f, 90f) * uvArray[i];
+			}
+			
+			mesh.uv = uvArray;
+			meshFilter.sharedMesh = mesh;
+			
+			EditorUtility.SetDirty(meshFilter);
+		}
+	}
+	
+	private void DrawToolButtons()
+	{
+		// Move Buttons
+		
+		
+		if (GUI.Button(new Rect(0f, 0f, 1f, 1f).Multiply(UISize) ,"R")) {
+			Refresh();
+		}
+		
+		Rect[] posRotButtons = RectEx.RectGrid(new Rect(1f, 1f, 9f, 9f), 3, 3);
+		
+		if (GUI.Button(posRotButtons[0].Multiply(UISize), new GUIContent("Rotate\nLeft", "Rotates selected objects counter-clockwise"))) {
+			RotateSelectedObjects(new Vector3(0f, -90f, 0f));
+		}
+		
+		if (GUI.Button(posRotButtons[1].Multiply(UISize), "+Z")) {
+			MoveSelectedObjects(new Vector3(0f, 0f, 1f));
+		}
+		
+		if (GUI.Button(posRotButtons[2].Multiply(UISize), "Rotate\nRigth")) {
+			RotateSelectedObjects(new Vector3(0f, 90f, 0f));
+		}
+		
+		
+		if (GUI.Button(posRotButtons[3].Multiply(UISize), "-X")) {
+			MoveSelectedObjects(new Vector3(-1f, 0f, 0f));
+		}
+		
+		if (GUI.Button(posRotButtons[4].Multiply(UISize), "Zero")) {
+			foreach( GameObject selected in Selection.gameObjects ) {
+				Vector3 currentPos = selected.transform.localPosition;
+				selected.transform.localPosition = new Vector3(0f, currentPos.y, 0f);
+			}
+		}
+		
+		if (GUI.Button(posRotButtons[5].Multiply(UISize), "+X")) {
+			MoveSelectedObjects(new Vector3(1f, 0f, 0f));
+		}
+		
+		if (GUI.Button(posRotButtons[6].Multiply(UISize), "-Y")) {
+			MoveSelectedObjects(new Vector3(0f, -1f, 0f));
+		}
+		
+		if (GUI.Button(posRotButtons[7].Multiply(UISize), "-Z")) {
+			MoveSelectedObjects(new Vector3(0f, 0f, -1f));
+		}
+		
+		if (GUI.Button(posRotButtons[8].Multiply(UISize), "+Y")) {
+			MoveSelectedObjects(new Vector3(0f, 1f, 0f));
+		}
+		
+		
+		Rect[] toolButtons = RectEx.RectGrid(new Rect(11f, 1f, 8f, 9f), 2, 4);
+		
+		if (GUI.Button(toolButtons[0].Multiply(UISize), "Rotate UV")) {
+			RotateSelectedUVs();
+		}
+		
+		GUI.enabled = false;
+		
+		if (GUI.Button(toolButtons[1].Multiply(UISize), "----")) {
+			
+		}
+		
+		if (GUI.Button(toolButtons[2].Multiply(UISize), "----")) {
+			
+		}
+		
+		if (GUI.Button(toolButtons[3].Multiply(UISize), "----")) {
+			
+		}
+		
+		if (GUI.Button(toolButtons[4].Multiply(UISize), "----")) {
+			
+		}
+		
+		if (GUI.Button(toolButtons[5].Multiply(UISize), "----")) {
+			
+		}
+		
+		if (GUI.Button(toolButtons[6].Multiply(UISize), "----")) {
+			
+		}
+		
+		if (GUI.Button(toolButtons[7].Multiply(UISize), "----")) {
+			
+		}
+		
+		GUI.enabled = true;
+		
+		
+	}
+	
+	private bool IsLevelInitialized()
+	{	
+		GameObject ligthGroup = GameObject.Find("LigthGroup");
+		GameObject levelController = GameObject.Find("LevelController");
+		GameObject gameManager = GameObject.Find("GameManager");
+		
+		if (ligthGroup == null || levelController == null || gameManager == null)
+			return false;
+		else
+			return true;
+	}
+	
+	private void InitializeLevel()
 	{
 		GameObject ligthGroup = GameObject.Find("LigthGroup");
 		GameObject levelController = GameObject.Find("LevelController");
 		GameObject gameManager = GameObject.Find("GameManager");
 		
+		if (ligthGroup == null)
+			PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath(@"Assets/Prefabs/Other/LigthGroup.prefab", typeof(GameObject)));
 		
-		if (ligthGroup == null || levelController == null || gameManager == null)
-		{
-			if (GUILayout.Button("INITIALIZE LEVEL"))
-			{
-				if (ligthGroup == null)
-					PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath(@"Assets/Prefabs/Other/LigthGroup.prefab", typeof(GameObject)));
-				
-				if (levelController == null)
-					PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath(@"Assets/Prefabs/Other/LevelController.prefab", typeof(GameObject)));
-				
-				if (gameManager == null)
-					PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath(@"Assets/Prefabs/Other/GameManager.prefab", typeof(GameObject)));
-			}
-			
-			GUI.enabled = false;
-		}
+		if (levelController == null)
+			PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath(@"Assets/Prefabs/Other/LevelController.prefab", typeof(GameObject)));
+		
+		if (gameManager == null)
+			PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath(@"Assets/Prefabs/Other/GameManager.prefab", typeof(GameObject)));
 	}
 	
 	private void DrawComponentButtons()
@@ -140,257 +294,53 @@ public class LevelEditor : EditorWindow {
 		
 	}
 	
-	private void DrawToolButtons()
-	{
-		// Move Buttons
-		float moveBtnSize = 50f;
-		GUILayout.BeginHorizontal();
+	private void DrawPrefabButton(Rect btnPos, PrefabItem prefabItem) {
 		
-		if( GUILayout.Button("Rotate\nLeft", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentRot = selected.transform.localEulerAngles;
-				float newRot = Mathf.Repeat(currentRot.y - 90f, 360f);
-				selected.transform.localEulerAngles = new Vector3(currentRot.x, Mathf.Round(newRot), currentRot.z);
-			}
-		}
-		
-		if( GUILayout.Button("Z+", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentPos = selected.transform.localPosition;
-				float newPos = Mathf.Round(currentPos.z + 1f);
-				selected.transform.localPosition = new Vector3(currentPos.x, currentPos.y, newPos);
-			}
-		}
-		
-		if( GUILayout.Button("Rotate\nRigth", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentRot = selected.transform.localEulerAngles;
-				float newRot = Mathf.Repeat(currentRot.y + 90f, 360f);
-				selected.transform.localEulerAngles = new Vector3(currentRot.x, Mathf.Round(newRot), currentRot.z);
-			}
-		}
-		
-		GUILayout.EndHorizontal();
-		
-		GUILayout.BeginHorizontal();
-		
-		if( GUILayout.Button("X-", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentPos = selected.transform.localPosition;
-				float newPos = Mathf.Round(currentPos.x - 1f);
-				selected.transform.localPosition = new Vector3(newPos, currentPos.y, currentPos.z);
-			}
-		}
-		
-		if( GUILayout.Button("Zero", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentPos = selected.transform.localPosition;
-				selected.transform.localPosition = new Vector3(0f, currentPos.y, 0f);
-			}
-		}
-		
-		if( GUILayout.Button("X+", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentPos = selected.transform.localPosition;
-				float newPos = Mathf.Round(currentPos.x + 1f);
-				selected.transform.localPosition = new Vector3(newPos, currentPos.y, currentPos.z);
-			}
-		}
-		
-		GUILayout.EndHorizontal();
-		
-		GUILayout.BeginHorizontal();
-		
-		if( GUILayout.Button("Y-", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentPos = selected.transform.localPosition;
-				float newPos = Mathf.Round(currentPos.y - 1f);
-				selected.transform.localPosition = new Vector3(currentPos.x, newPos, currentPos.z);
-			}
-		}
-		
-		if( GUILayout.Button("Z-", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentPos = selected.transform.localPosition;
-				float newPos = Mathf.Round(currentPos.z - 1f);
-				selected.transform.localPosition = new Vector3(currentPos.x, currentPos.y, newPos);
-			}
-		}
-		
-		if( GUILayout.Button("Y+", GUILayout.Width(moveBtnSize), GUILayout.Height(moveBtnSize)) )
-		{
-			foreach( GameObject selected in Selection.gameObjects )
-			{
-				Vector3 currentPos = selected.transform.localPosition;
-				float newPos = Mathf.Round(currentPos.y + 1f);
-				selected.transform.localPosition = new Vector3(currentPos.x, newPos, currentPos.z);
-			}
-		}
-		
-		GUILayout.EndHorizontal();
-		
-		GUILayout.BeginHorizontal();
-		
-		if( GUILayout.Button("Rotate UV", GUILayout.Width(70f)) )
-		{
-			Undo.RegisterUndo(Selection.gameObjects, "Rotate UVs");
+		if (GUI.Button(btnPos, new GUIContent(prefabItem.icon))) {
 			
-			foreach( GameObject go in Selection.gameObjects )
-			{
-				MeshFilter meshFilter = go.GetComponentInChildren<MeshFilter>();
-				Mesh mesh = (Mesh)Instantiate(meshFilter.sharedMesh);
-				
-				Vector2[] uvArray = mesh.uv;
-				
-				for (int i = 0; i < uvArray.Length; i++)
-				{
-					uvArray[i] = Quaternion.Euler(0f, 0f, 90f) * uvArray[i];
-				}
-				
-				mesh.uv = uvArray;
-				meshFilter.sharedMesh = mesh;
-				
-				EditorUtility.SetDirty(meshFilter);
+			GameObject itemParent = GameObject.Find("/" + prefabItem.parent);
+			
+			if(itemParent == null) { // Instantiate new parent object if none was found in the scene.
+				itemParent = new GameObject(prefabItem.parent);
 			}
+			
+			GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefabItem.prefab);
+			go.transform.parent = itemParent.transform;
+			
+			Vector3 itemPos = Vector3.zero;
+			
+			if(Selection.gameObjects.Length > 0) { // Spawn the new object at the position of the selected object if there is one.
+				itemPos = Selection.gameObjects[0].transform.position;
+			}
+			
+			go.transform.position = itemPos;
+			Selection.objects = new Object[]{go}; // Select the newly created object.
 		}
+		GUI.Label(btnPos, new GUIContent(prefabItem.itemName));
+	}
+	
+	private void DrawItemButtons() {
+		float buttonSize = 3f;
+		int btnCount = itemPrefabs.Count;
+		int rowCount = Mathf.CeilToInt((float)btnCount / 6f);
 		
-		GUILayout.EndHorizontal();
+		Rect[] itemBtnPosArray = RectEx.RectGrid(new Rect(1f, 13f, 18f, 3f * (float)rowCount), 6, rowCount);
 		
-		if(GUILayout.Button("Refresh", GUILayout.Width(70f)))
-		{
-			Refresh();
+		for (int i = 0; i < btnCount; i++) {
+			DrawPrefabButton(itemBtnPosArray[i].Multiply(UISize), itemPrefabs[i]);
 		}
 	}
 	
-	private void DrawItemButtons()
-	{
-		GUILayout.Space(20f);
-		GUILayout.Label("Items");
-		GUILayout.BeginHorizontal();
+	private void DrawLevelButtons() {
+		float buttonSize = 3f;
+		int btnCount = levelPrefabs.Count;
+		int rowCount = Mathf.CeilToInt((float)btnCount / 6f);
 		
-		float buttonSize = 60f;
-		int btni = 0;
+		Rect[] itemBtnPosArray = RectEx.RectGrid(new Rect(1f, 13f, 18f, 3f * (float)rowCount), 6, rowCount);
 		
-		int btnPerRow = Mathf.FloorToInt(window.position.width / buttonSize);
-		
-		for ( int i = 0; i < itemPrefabs.Count; i++ )
-		{
-			LevelItem levelItem = itemPrefabs[i];
-			
-			GUILayout.BeginVertical();
-			
-			if( GUILayout.Button(levelItem.icon, GUILayout.Height(buttonSize), GUILayout.Width(buttonSize)) )
-			{
-				string itemParentName = "items";
-				GameObject itemParent = GameObject.Find("/" + itemParentName);
-				
-				if(itemParent == null) // Instantiate new parent object if none was found in the scene.
-				{
-					itemParent = new GameObject(itemParentName);
-				}
-				
-				GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(levelItem.prefab);
-				go.transform.parent = itemParent.transform;
-				
-				Vector3 itemPos = Vector3.zero;
-				
-				if(Selection.gameObjects.Length > 0)
-				{
-					itemPos = Selection.gameObjects[0].transform.position;
-				}
-				
-				go.transform.position = itemPos;
-				
-				Selection.objects = new Object[]{go}; // Select newly created object
-			}
-			GUILayout.Label(levelItem.itemName, GUILayout.Width(buttonSize));
-			
-			GUILayout.EndVertical();
-			btni++;
-			
-			if(btni >= btnPerRow)
-			{
-				GUILayout.EndHorizontal();
-				GUILayout.BeginHorizontal();
-				btni = 0;
-			}
+		for (int i = 0; i < btnCount; i++) {
+			DrawPrefabButton(itemBtnPosArray[i].Multiply(UISize), levelPrefabs[i]);
 		}
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
-	}
-	
-	private void DrawLevelButtons()
-	{
-		// Level prefab buttons
-		GUILayout.Label("Level");
-		GUILayout.BeginHorizontal();
-		
-		float buttonSize = 60f;
-		int btni = 0;
-		int btnPerRow = Mathf.FloorToInt(window.position.width / (buttonSize + 3f));
-		
-		for ( int i = 0; i < levelPrefabs.Count; i++ )
-		{
-			LevelItem levelItem = levelPrefabs[i];
-			
-			GUILayout.BeginVertical();
-			
-			if( GUILayout.Button(levelItem.icon, GUILayout.Height(buttonSize), GUILayout.Width(buttonSize)) )
-			{
-				string itemParentName = "level";
-				GameObject itemParent = GameObject.Find("/" + itemParentName);
-				
-				if(itemParent == null) // Instantiate new parent object if none was found in the scene.
-				{
-					itemParent = new GameObject(itemParentName);
-				}
-				
-				GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(levelItem.prefab);
-				go.transform.parent = itemParent.transform;
-				
-				Vector3 itemPos = Vector3.zero;
-				
-				if(Selection.gameObjects.Length > 0)
-				{
-					itemPos = Selection.gameObjects[0].transform.position;
-				}
-				
-				go.transform.position = itemPos;
-				
-				Selection.objects = new Object[]{go}; // Select newly created object
-			}
-			
-			GUILayout.Label(levelItem.itemName, GUILayout.Width(buttonSize));
-			
-			GUILayout.EndVertical();
-			
-			btni++;
-			
-			if(btni >= btnPerRow)
-			{
-				GUILayout.EndHorizontal();
-				GUILayout.BeginHorizontal();
-				btni = 0;
-			}
-		}
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
 	}
 	
 }
