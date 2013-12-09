971bc3f1b95deb2b21df01fc309ca8c7a9306c50
Matias Christensen <mattivc@gmail.com>
2013-12-09 16:01:35 +0100

    Fixed animation of Sawblade

---
+++ Assets/SawbladeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SawbladeController : MonoBehaviour {
+
+	public float speed = 180.0f;
+
+	private float rotation = 0.0f;
+	private Transform meshTransform;
+
+	void Awake() {
+		meshTransform = transform.GetChild(0);
+	}
+
+	void Update() {
+		rotation += speed * Time.deltaTime;
+		rotation = Mathf.Repeat( rotation, 360.0f );
+		meshTransform.localEulerAngles = new Vector3( 0.0f, rotation, 0.0f );
+	}
+
+}
